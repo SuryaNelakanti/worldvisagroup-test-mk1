2b199b2d11c31e69b2c602573a0d1988cda0a081
Suryateja Nelakanti <47491728+SuryaNelakanti@users.noreply.github.com>
2021-06-28 14:37:33 +0530

    Add files via upload

---
+++ scipts/scripts.js
@@ -0,0 +1,46 @@
+window.onscroll = function(){scrollFunction()};
+function scrollFunction(){
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 80) {
+        //Changing:
+        //->nav bar background colour to white on scroll
+        document.getElementById("nav").style.background="#FFFF";
+        //->Adding box shadow
+        document.getElementById("nav").style.boxShadow="0px 0px 5px 0px rgba(0,0,0,0.75)";
+        //->Calling function that changes text-colour of the navitem
+        colorBlack(document.querySelectorAll("nav ul li a"));
+        //->Changing colour of the hamburger menu
+        document.getElementById('j-point').style.color="#000";
+        //->Changing logo from white background to original
+        document.getElementById('logo-main').src="./images/logo/logo1.svg";
+        //Adding animations
+        animationAdder();
+    } else{
+        document.getElementById("nav").style.background="transparent";
+        document.getElementById("nav").style.boxShadow="0 0 0 0 white";
+        colorWhite(document.querySelectorAll("nav ul li a"));
+        document.getElementById('j-point').style.color="#fff";
+        document.getElementById('logo-main').src="./images/logo/logo1_white.svg";
+    }
+}
+function colorBlack(array){
+  for(let i=0;i<6;i++){
+      array[i].style.color="#000";
+  }
+}
+function colorWhite(array){
+    for(let i=0;i<6;i++){
+      array[i].style.color="#fff";
+  }
+}
+
+function animationAdder(){  
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 160){
+        //Adding animation: 
+    let element = document.querySelector('.what-we-do');
+    element.classList.add('animate__animated', 'animate__zoomInUp');
+    } else if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 200){
+    }
+}
+let elementTwo = document.querySelector('.button-container');
+console.log(elementTwo)
+elementTwo.classList.add('animate__animated', 'animate__zoomInUp'); 
